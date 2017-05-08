35a176e3a7ff7423c9dfb41555281a954c7ce84b
HB <hb@localhost.localdomain>
2017-05-08 15:59:05 +0930

    Added very light argument validation

--- challenges_main/Challenges_main.java
+++ challenges_main/Challenges_main.java
@@ -5,7 +5,13 @@ import name_generator.*;
 /** Main class for executing the Classes in which challenges are implemented/solved */
 public class Challenges_main {
 	public static void main(String[] args){
-		NameGenerator nameGen = new NameGenerator(); //NameGenerator has no fancy constructor
-		nameGen.generate(args);
+		NameGenerator nameGen;
+		try {
+			nameGen = new NameGenerator(args);
+			nameGen.generate();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} //NameGenerator has no fancy constructor
 	}
 }
--- name_generator/NameGenerator.java
+++ name_generator/NameGenerator.java
@@ -1,15 +1,39 @@
 package name_generator;
 
+/** Reads in first names, last names, street addresses, and mobile phone numbers from files indicated by
+ * 	their names which are passed in as arguments. The desired number of detail sets (names) to be generated is passed in
+ * 	as an optional argument at the end of args.
+ * @author hb
+ *
+ */
 public class NameGenerator {
-
-	public void generate(String[] args) {
-		// TODO Auto-generated method stub
-		validateArgs(args);
+	private static int MIN_EXPECTED_ARGS = 4;
+	private static int MAX_EXPECTED_ARGS = 5;
+	private static int DEFAULT_NAME_QUOTA = 15;
+	private String args[];
+	
+	public NameGenerator(String args[]) throws Exception
+	{
+		if(validateArgs(args))
+		{
+			this.args = args;
+		}
+		else
+		{
+			throw new Exception("Arguments invalid, I'd write a more helpful message but I won't");
+		}
+	}
+	
+	public void generate()
+	{
+		//This is going to end up as a monolithic method
 		
 	}
 	
-	private boolean validateArgs(String[] args){
-		return false;
+	private boolean validateArgs(String[] args)
+	{
+		//args[4] being the optional user entered name quota
+		return ((args.length < MIN_EXPECTED_ARGS || args.length > MAX_EXPECTED_ARGS) || !(args[4].matches("[0-9]+")));
 	}
 	
 
