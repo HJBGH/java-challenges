2402c4add85e34706bd55504b93a9c5a44a1b489
HB <HJBowland@protonmail.com>
2017-05-17 16:44:40 +0930

    higher lower game working, buggy as hell replay function working. I will have to restructure this program

--- higher_lower/HigherLower.java
+++ higher_lower/HigherLower.java
@@ -11,13 +11,14 @@ import java.util.Scanner;
 public class HigherLower {
 	//total number of turns the player has to guess the number
 	private static int TURNS = 10;
-	private static Random rand = new Random();
-	private static int TARGET = rand.nextInt(99) + 1;// add 1 because you can't change the lower limit.
+	private Random rand = new Random();
+	//does this count as abusing globals?
 	private static Scanner inputScanner = new Scanner(System.in);
 	
 	//this is going to be doing everything, it really shouldn't
 	public void play()
 	{
+		int target = rand.nextInt(99) + 1;// add 1 because you can't change the lower limit.
 		int turn = 0;
 		int guess;
 		System.out.println("Guess the number between 1 and 100, you have 10 guesses.");
@@ -28,7 +29,24 @@ public class HigherLower {
 			try
 			{
 				guess = inputScanner.nextInt();
+				if(guess < target)
+					System.out.println("Higher.");
+				if(guess > target)
+					System.out.println("Lower.");
 				turn++;
+				if(guess == target)
+				{
+					System.out.println("You got it!");
+					if(playAgain() == true)
+					{
+						turn = 0;
+					}
+					else
+					{
+					return;
+					}
+				}
+				
 			}
 			catch (Exception e)
 			{
@@ -37,14 +55,31 @@ public class HigherLower {
 			}
 			
 		}
+		//if it gets down here then the player hasn't won
+		System.out.println("You fucked up!");
+		
 		inputScanner.close();
 		return;
 	}
 	
-	//no point defining custom exceptions
-	private void validateInput(String input) throws Exception
+	private boolean playAgain()
 	{
-		
+		System.out.println("Play again? y/n");
+		System.out.print(">");
+		while(true)
+		{
+			if(((inputScanner.nextLine()).toLowerCase()).equals("y")){
+				return true;
+			}
+			if(((inputScanner.nextLine()).toLowerCase()).equals("n")){
+				return false;
+			}
+			System.out.println("Bad input, play again? y/n");
+			System.out.print(">");
+			inputScanner.nextLine();
+		}
+		 //Is it bad convention to have this here?
+
 	}
 
 }
