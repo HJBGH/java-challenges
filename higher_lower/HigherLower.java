2b8125307e56cacb03adbb2ed21b2d67275b5e45
HB <HJBowland@protonmail.com>
2017-05-17 16:04:27 +0930

    now handling erroneous input correctly

--- higher_lower/HigherLower.java
+++ higher_lower/HigherLower.java
@@ -1,6 +1,7 @@
 package higher_lower;
 
 import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A Higher-Lower number guessing game, the machine picks a number between 1 and 100 inclusive and the player has TURNS number of turns to guess it.
@@ -12,28 +13,31 @@ public class HigherLower {
 	private static int TURNS = 10;
 	private static Random rand = new Random();
 	private static int TARGET = rand.nextInt(99) + 1;// add 1 because you can't change the lower limit.
+	private static Scanner inputScanner = new Scanner(System.in);
 	
 	//this is going to be doing everything, it really shouldn't
 	public void play()
 	{
 		int turn = 0;
-		System.out.println("Guess the number between 1 and 100");
+		int guess;
+		System.out.println("Guess the number between 1 and 100, you have 10 guesses.");
 		while (turn < TURNS)
 		{
-			System.out.print(">");
+			System.out.printf("%02d%s", TURNS-turn, ">");
 			//get input
 			try
 			{
-				
+				guess = inputScanner.nextInt();
+				turn++;
 			}
 			catch (Exception e)
 			{
-				
+				System.out.println("Incorrect input, please enter an integer between 1 and 100 inclusive.");
+				inputScanner.nextLine();
 			}
-			System.out.println("FUCK YOU " + turn);
-			//input goes here
-			turn++;
+			
 		}
+		inputScanner.close();
 		return;
 	}
 	
